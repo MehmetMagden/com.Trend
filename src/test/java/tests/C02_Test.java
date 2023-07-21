6ce1e976374ed61a94186400bbf29fe48108d24a
Mehmet Maden <yasamtr@gmail.com>
2023-07-22 02:28:55 +0300

    thistime

--- src/test/java/tests/C02_Test.java
+++ src/test/java/tests/C02_Test.java
@@ -1,7 +1,18 @@
 package tests;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class C02_Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        List<WebElement> testList = new ArrayList<>();
+
+        for (WebElement each: testList
+             ) {
+
+        }
     }
 
 }
